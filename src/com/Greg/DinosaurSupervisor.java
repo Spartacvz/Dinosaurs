1206dae606d819b8fd7a9223bcf6d544ed7541c8
Spartacvz <oc6144yh@go.minneapolis.edu>
2017-02-15 17:48:35 -0600

    Dinosaur GIT

--- src/com/Greg/DinosaurSupervisor.java
+++ src/com/Greg/DinosaurSupervisor.java
@@ -1,7 +1,14 @@
 package com.Greg;
 
-/**
- * Created by dadub on 2/15/2017.
- */
-public class DinosaurSupervisor {
-}
+
+
+    public class DinosaurSupervisor {
+        public static void main(String[] args) {
+
+            Dinosaur t_rex = new Dinosaur();
+            Dinosaur velociraptor = new Dinosaur();
+
+            t_rex.roar();
+            velociraptor.roar();
+        }
+    }
